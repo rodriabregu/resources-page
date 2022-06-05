f9504befe0f8604ec8814e0e012297f4668e2b85
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-06-05 02:58:42 -0300

    feat/config-theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme, ColorModeScript, type ThemeConfig } from '@chakra-ui/react';
 
 import App from './App';
 import './index.css';
@@ -13,11 +13,14 @@ const colors = {
   },
 };
 
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  disableTransitionOnChange: false,
+}
+
 const theme = extendTheme({
   colors,
-  config: {
-    disableTransitionOnChange: false,
-  },
+  config,
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
