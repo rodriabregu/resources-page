f8155f6eafe618a993fe5f91bbf0d4d6da8c4f4e
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-06-05 03:01:05 -0300

    feat/refactor-layout

--- src/components/ResourceLayout.tsx
+++ src/components/ResourceLayout.tsx
@@ -22,8 +22,18 @@ const ResourceLayout = ({
   data: ResourceData[];
 }) => {
   const { colorMode } = useColorMode();
+  const settingColor = colorMode === 'light' ? 'rgba(25,0,0,0.1)' : '#27272a';
   return (
-    <Box p={'2'} bg={'rgba(25,0,0,0.1)'} borderRadius={'4'}>
+    <Box
+      m={'2'}
+      ml={{ base: '0', md: '2' }}
+      mr={{ base: '0', md: '2' }}
+      p={'2'}
+      bg={settingColor}
+      borderRadius={'4'}
+      w={'100'}
+      id={title}
+    >
       <Center>
         <Tag>{title}</Tag>
       </Center>
@@ -34,8 +44,12 @@ const ResourceLayout = ({
               <Text
                 m={'1'}
                 borderRadius={'4'}
-                bg={colorMode === 'light' ? '#DBDBDB' : '#2A2A2A'}
-                noOfLines={2}
+                noOfLines={1}
+                w={{ base: '100%', md: '80' }}
+                _hover={{
+                  backgroundColor:
+                    colorMode === 'light' ? '#DBDBDB' : 'rgba(5,0,0,0.1)',
+                }}
               >
                 <LinkIcon /> {item.name}
               </Text>
