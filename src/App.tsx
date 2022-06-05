519a27665962960549b0a37024b6692e3805a62b
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-06-05 02:59:46 -0300

    feat/initial-layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,124 @@
-import './App.css';
 import {
-  Button,
   useColorMode,
-  Tag,
-  Heading,
-  Text,
-  Link,
-  Box,
-  Container,
+  GridItem,
   Center,
-  Flex,
-  VStack,
-  Stack,
-  HStack,
-  Divider,
-  StackDivider,
+  Button,
   Grid,
-  SimpleGrid,
+  Flex,
+  Text,
+  Box,
 } from '@chakra-ui/react';
-import { LinkIcon } from '@chakra-ui/icons';
+import AlertIntro from './components/AlertIntro';
+
+import ResourceLayout from './components/ResourceLayout';
+import {
+  dataGithub,
+  dataFrontend,
+  dataBackend,
+  dataRoadmap,
+  dataJavascript,
+  dataLearning,
+  dataBootcamp,
+} from './data/example';
+
+const titles = [
+  'Bootcamps',
+  'Github',
+  'Roadmap',
+  'Frontend',
+  'Backend',
+  'Javascript',
+  'Learning',
+];
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const settingTheme = colorMode !== 'light' ? 'Light' : 'Dark';
+  const settingColor = colorMode === 'light' ? 'rgba(25,0,0,0.1)' : '#27272a';
+
+  const scrollToSection = (title: string) =>
+    (document.getElementById(title) as HTMLDivElement).scrollIntoView({
+      behavior: 'smooth',
+    });
 
   return (
-    <Container>
-      <Heading paddingTop={'5'}>I'm a heading</Heading>
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-      </Button>
-      <SimpleGrid minChildWidth={'40'} spacing='10px'>
-        <Box>
-          <Center>
-            <Tag>Bootcamps</Tag>
-          </Center>
-          <VStack>
-            <Link>
-              <LinkIcon /> SoyHenry
-            </Link>
-            <Link>
-              <LinkIcon /> OpenBootcamp
-            </Link>
-            <Link>
-              <LinkIcon /> Midudev Bootcamp
-            </Link>
-          </VStack>
-        </Box>
-        <Divider />
-        <Box>
-          <Center>
-            <Tag>Videos2</Tag>
-          </Center>
-          <VStack>
-            <Link>
-              <LinkIcon /> SoyHenry2
-            </Link>
-            <Link>
-              <LinkIcon /> OpenBootcamp2
-            </Link>
-            <Link>
-              <LinkIcon /> Midudev Bootcamp2
-            </Link>
-          </VStack>
-        </Box>
-        <Divider />
-        <Box>
-          <Center>
-            <Tag>Courses</Tag>
-          </Center>
-          <VStack>
-            <Link>
-              <LinkIcon /> SoyHenry
-            </Link>
-            <Link>
-              <LinkIcon /> OpenBootcamp
-            </Link>
-          </VStack>
-        </Box>
-        <Divider />
-        <Box>
-          <Center>
-            <Tag>Learning</Tag>
-          </Center>
-          <VStack>
-            <Link>
-              <LinkIcon /> SoyHenry
-            </Link>
-            <Link>
-              <LinkIcon /> OpenBootcamp
-            </Link>
-          </VStack>
-        </Box>
-      </SimpleGrid>
-    </Container>
+    <Box>
+      <Center>
+        <Button m={'4'} onClick={toggleColorMode}>
+          Toggle {settingTheme}
+        </Button>
+      </Center>
+      
+      <AlertIntro />
+
+      <Center>
+        <Grid
+          templateColumns='repeat(4, 1fr)'
+          justifyItems={'center'}
+          borderRadius={'4'}
+          bg={settingColor}
+          gap={2}
+          w={{ base: '100%', md: 'auto' }}
+        >
+          {titles.map((title) => (
+            <GridItem w='auto' h='10'>
+              <Text
+                key={title}
+                borderRadius={'4'}
+                cursor={'pointer'}
+                m={'1'}
+                p={'1'}
+                onClick={() => scrollToSection(title)}
+                _hover={{
+                  backgroundColor:
+                    colorMode === 'light' ? '#DBDBDB' : 'rgba(5,0,0,0.1)',
+                }}
+              >
+                {title}
+              </Text>
+            </GridItem>
+          ))}
+        </Grid>
+      </Center>
+
+
+
+      <Flex
+        // p={'4'}
+        // m={'4'}
+        // display={'inline-block'}
+        // display={'table'}
+        // w={'100%'}
+        // w={'80vw'}
+        align={'center'}
+        direction={'column'}
+      >
+        <Flex flexDir={{ base: 'column', md: 'row' }}>
+          <ResourceLayout title={'Learning'} data={dataLearning} />
+          <ResourceLayout title={'Javascript'} data={dataJavascript} />
+        </Flex>
+
+        <Flex flexDir={{ base: 'column', md: 'row' }}>
+          <ResourceLayout title={'Javascript'} data={dataJavascript} />
+          <ResourceLayout title={'Javascript'} data={dataJavascript} />
+        </Flex>
+
+        <Flex flexDir={{ base: 'column', md: 'row' }}>
+          <ResourceLayout title={'Bootcamps'} data={dataBootcamp} />
+          <ResourceLayout title={'Github'} data={dataGithub} />
+        </Flex>
+
+        <Flex flexDir={{ base: 'column', md: 'row' }}>
+          <ResourceLayout title={'Assets'} data={dataBootcamp} />
+          <ResourceLayout title={'Roadmap'} data={dataRoadmap} />
+        </Flex>
+
+        <Flex flexDir={{ base: 'column', md: 'row' }}>
+          <ResourceLayout title={'Backend'} data={dataBackend} />
+          <ResourceLayout title={'Frontend'} data={dataFrontend} />
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
 
