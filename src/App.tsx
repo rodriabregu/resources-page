f5187dadbbfbaabffc45f1431ac2398c67e2c71b
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-06-11 02:18:21 -0300

    feat/home-refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,123 +1,54 @@
+import { Box, Flex } from '@chakra-ui/react'
 import {
-  useColorMode,
-  GridItem,
-  Center,
-  Button,
-  Grid,
-  Flex,
-  Text,
-  Box,
-} from '@chakra-ui/react';
-import AlertIntro from './components/AlertIntro';
-
-import ResourceLayout from './components/ResourceLayout';
-import {
-  dataGithub,
-  dataFrontend,
   dataBackend,
-  dataRoadmap,
+  dataBootcamp,
+  dataFrontend,
+  dataGithub,
   dataJavascript,
   dataLearning,
-  dataBootcamp,
-} from './data/example';
+  dataRoadmap
+} from 'data/example'
 
-const titles = [
-  'Bootcamps',
-  'Github',
-  'Roadmap',
-  'Frontend',
-  'Backend',
-  'Javascript',
-  'Learning',
-];
+import AlertIntro from 'components/AlertIntro'
+import ButtonDarkMode from 'components/ButtonDarkMode'
+import ResourceLayout from 'components/Layout/ResourceLayout'
+import WrapperLayout from 'components/Layout/WrapperLayout'
+import SummarySection from 'components/SummarySection'
 
 function App() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const settingTheme = colorMode !== 'light' ? 'Light' : 'Dark';
-  const settingColor = colorMode === 'light' ? 'rgba(25,0,0,0.1)' : '#27272a';
-
-  const scrollToSection = (title: string) =>
-    (document.getElementById(title) as HTMLDivElement).scrollIntoView({
-      behavior: 'smooth',
-    });
-
   return (
     <Box>
-      <Center>
-        <Button m={'4'} onClick={toggleColorMode}>
-          Toggle {settingTheme}
-        </Button>
-      </Center>
-      
+      <ButtonDarkMode />
       <AlertIntro />
-
-      <Center>
-        <Grid
-          templateColumns='repeat(4, 1fr)'
-          justifyItems={'center'}
-          borderRadius={'4'}
-          bg={settingColor}
-          gap={2}
-          w={{ base: '100%', md: 'auto' }}
-        >
-          {titles.map((title) => (
-            <GridItem w='auto' h='10'>
-              <Text
-                key={title}
-                borderRadius={'4'}
-                cursor={'pointer'}
-                m={'1'}
-                p={'1'}
-                onClick={() => scrollToSection(title)}
-                _hover={{
-                  backgroundColor:
-                    colorMode === 'light' ? '#DBDBDB' : 'rgba(5,0,0,0.1)',
-                }}
-              >
-                {title}
-              </Text>
-            </GridItem>
-          ))}
-        </Grid>
-      </Center>
-
-      <Flex
-        // p={'4'}
-        // m={'4'}
-        // display={'inline-block'}
-        // display={'table'}
-        // w={'100%'}
-        // w={'80vw'}
-        align={'center'}
-        direction={'column'}
-      >
-        <Flex flexDir={{ base: 'column', md: 'row' }}>
+      <SummarySection />
+      <Flex m={'4'} align={{ md: 'center' }} direction={'column'}>
+        <WrapperLayout>
           <ResourceLayout title={'Learning'} data={dataLearning} />
           <ResourceLayout title={'Javascript'} data={dataJavascript} />
-        </Flex>
+        </WrapperLayout>
 
-        <Flex flexDir={{ base: 'column', md: 'row' }}>
+        <WrapperLayout>
           <ResourceLayout title={'Javascript'} data={dataJavascript} />
           <ResourceLayout title={'Javascript'} data={dataJavascript} />
-        </Flex>
+        </WrapperLayout>
 
-        <Flex flexDir={{ base: 'column', md: 'row' }}>
+        <WrapperLayout>
           <ResourceLayout title={'Bootcamps'} data={dataBootcamp} />
           <ResourceLayout title={'Github'} data={dataGithub} />
-        </Flex>
+        </WrapperLayout>
 
-        <Flex flexDir={{ base: 'column', md: 'row' }}>
+        <WrapperLayout>
           <ResourceLayout title={'Assets'} data={dataBootcamp} />
           <ResourceLayout title={'Roadmap'} data={dataRoadmap} />
-        </Flex>
+        </WrapperLayout>
 
-        <Flex flexDir={{ base: 'column', md: 'row' }}>
+        <WrapperLayout>
           <ResourceLayout title={'Backend'} data={dataBackend} />
           <ResourceLayout title={'Frontend'} data={dataFrontend} />
-        </Flex>
+        </WrapperLayout>
       </Flex>
     </Box>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider, extendTheme, ColorModeScript, type ThemeConfig } from '@chakra-ui/react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { type ThemeConfig, ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
 
-import App from './App';
-import './index.css';
+import App from './App'
 
 const colors = {
   brand: {
     900: '#1a365d',
     800: '#153e75',
-    700: '#2a69ac',
+    700: '#2a69ac'
   },
-};
+  bluePrimary: 'rgba(25,0,0,0.1)'
+}
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
-  disableTransitionOnChange: false,
+  disableTransitionOnChange: false
 }
 
 const theme = extendTheme({
   colors,
-  config,
-});
+  config
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -30,4 +30,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </ChakraProvider>
   </React.StrictMode>
-);
+)
