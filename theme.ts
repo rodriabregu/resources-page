55a08479ced1a91b44fff526a4c34a16f398756b
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-06-16 23:13:53 -0300

    feat/theme

--- theme.ts
+++ theme.ts
@@ -1,5 +1,5 @@
 // theme.js
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
   initialColorMode: 'light',
@@ -8,29 +8,29 @@ const theme = extendTheme({
     Button: {
       // 1. We can update the base styles
       baseStyle: {
-        fontWeight: 'bold', // Normally, it is "semibold"
+        fontWeight: 'bold' // Normally, it is "semibold"
       },
       // 2. We can add a new button size or extend existing
       sizes: {
         xl: {
           h: '56px',
           fontSize: 'lg',
-          px: '32px',
-        },
+          px: '32px'
+        }
       },
       // 3. We can add a new visual variant
       variants: {
         'with-shadow': {
           bg: 'red.400',
-          boxShadow: '0 0 2px 2px #efdfde',
+          boxShadow: '0 0 2px 2px #efdfde'
         },
         // 4. We can override existing variants
-        solid: (props) => ({
-          bg: props.colorMode === 'dark' ? 'red.300' : 'red.500',
-        }),
-      },
-    },
-  },
-});
+        solid: props => ({
+          bg: props.colorMode === 'dark' ? 'red.300' : 'red.500'
+        })
+      }
+    }
+  }
+})
 
-export default theme;
+export default theme
