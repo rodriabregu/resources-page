b5b2d409c62f496265c3d77bda1bdcbb2dff776f
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-06-17 20:29:26 -0300

    feat/resourcings

---
+++ src/data/resourcing.ts
@@ -0,0 +1,419 @@
+export const dataBootcamp = [
+  { name: 'SoyHenry 💰 / 🆓', link: 'https://soyhenry.com' },
+  { name: 'Full stack open 2022 🆓', link: 'https://fullstackopen.com/es/' },
+  {
+    name: 'Open Bootcamp 💰 / 🆓',
+    link: 'https://open-bootcamp.com/cursos/react'
+  },
+  {
+    name: 'Plataforma 5 💰',
+    link: 'https://www.plataforma5.la/'
+  }
+]
+
+export const dataGithub = [
+  { name: 'Public Apis', link: 'https://github.com/public-apis/public-apis' },
+  {
+    name: 'Clean code JS (buenas prácticas) 📖',
+    link: 'https://github.com/devictoribero/clean-code-javascript'
+  },
+  {
+    name: 'El camino del Frontend Developer',
+    link: 'https://github.com/mrcodedev/frontend-developer-resources'
+  },
+  {
+    name: 'Colección de readmes para Github/profile',
+    link: 'https://github.com/durgeshsamariya/awesome-github-profile-readme-templates'
+  },
+  {
+    name: '10 días de Python',
+    link: 'https://github.com/Asabeneh/10DaysOfPython'
+  },
+  {
+    name: '30 días de Python',
+    link: 'https://github.com/Asabeneh/30-Days-Of-Python'
+  },
+  {
+    name: 'Colección bibliotecas Javascript',
+    link: 'https://github.com/sorrycc/awesome-javascript'
+  },
+  {
+    name: 'Colección repositorios por Stars github',
+    link: 'https://github.com/maguowei/awesome-stars#javascript'
+  }
+]
+
+export const dataFrontend = [
+  {
+    name: 'CSS Cheatsheet (the best)',
+    link: 'https://htmlcheatsheet.com/css/'
+  },
+  {
+    name: 'Paletas de colores',
+    link: 'https://coolors.co/'
+  },
+  {
+    name: 'Escuela frontend',
+    link: 'https://www.escuelafrontend.com/'
+  },
+  {
+    name: 'React.js cheatsheet',
+    link: 'https://devhints.io/react'
+  },
+  {
+    name: 'Paleta de colores (paletas random)',
+    link: 'https://picture-palette.web.app/'
+  },
+  {
+    name: 'Paleta de color personalizable',
+    link: 'https://colorable.jxnblk.com/b3aab0/49524c'
+  },
+  {
+    name: 'Fancy-Border-Radius (figuras locas con b.r.)',
+    link: 'https://9elements.github.io/fancy-border-radius/'
+  },
+  {
+    name: 'Shapes (estilos con ondas)',
+    link: 'https://www.shapedivider.app/'
+  }
+]
+
+export const dataBackend = [
+  {
+    name: 'Any Api (apis gratuitas)',
+    link: 'https://any-api.com/'
+  },
+  {
+    name: 'HTTP Cats (códigos http con michis) 📖',
+    link: 'https://http.cat/'
+  },
+  {
+    name: 'Sequelize documentación 📖',
+    link: 'https://sequelize.org/v5/manual/data-types.html'
+  },
+  {
+    name: 'PostgreSQL documentación 📖',
+    link: 'https://www.postgresqltutorial.com/postgresql-getting-started/'
+  }
+]
+
+export const dataRoadmap = [
+  {
+    name: 'Frontend Roadmap',
+    link: 'https://frontendmasters.com/guides/learning-roadmap/'
+  },
+  {
+    name: 'Roadmaps de TODO',
+    link: 'https://roadmap.sh/'
+  }
+]
+
+export const dataJavascript = [
+  {
+    name: 'Curso de Javascript (John Mircha) 🎥',
+    link: 'https://aprendejavascript.org/'
+  },
+  {
+    name: 'Curso Javascript moderno 📖',
+    link: 'https://es.javascript.info/'
+  },
+  {
+    name: 'Javascript WTF (cosas ilógicas)',
+    link: 'https://javascriptwtf.com/'
+  },
+  {
+    name: '30 días de Javascript (tareas diarias) 📖',
+    link: 'https://github.com/Asabeneh/30-Days-Of-JavaScript'
+  },
+  {
+    name: '33 Conceptos que todo desarrollador de JavaScript debería saber 📖',
+    link: 'https://github.com/adonismendozaperez/33-js-conceptos'
+  }
+]
+
+export const dataLearning = [
+  {
+    name: 'Platzi 💰',
+    link: 'https://platzi.com/home'
+  },
+  {
+    name: 'Udemy 💰 / 🆓',
+    link: 'https://www.udemy.com/'
+  },
+  {
+    name: 'Coderhouse 💰',
+    link: 'https://www.coderhouse.com/'
+  },
+  {
+    name: 'Digitalhouse 💰',
+    link: 'https://www.digitalhouse.com/'
+  },
+  {
+    name: 'FreeCodeCamp 🆓',
+    link: 'https://www.freecodecamp.org/'
+  },
+  {
+    name: '#Javascript30 🆓 - 🎥',
+    link: 'https://javascript30.com/'
+  },
+  {
+    name: 'W3schools',
+    link: 'https://www.w3schools.com/'
+  },
+  {
+    name: 'Codecademy 💰 / 🆓',
+    link: 'https://www.codecademy.com/'
+  },
+  {
+    name: 'Test Automation University 🆓',
+    link: 'https://testautomationu.applitools.com/'
+  },
+  {
+    name: 'SoloLearn 💰 / 🆓',
+    link: 'https://www.sololearn.com/home'
+  }
+]
+
+export const dataHosting = [
+  {
+    name: 'Vercel 🆓',
+    link: 'https://vercel.com/'
+  },
+  {
+    name: 'GithubPages 🆓',
+    link: 'https://pages.github.com/'
+  },
+  {
+    name: 'Netlify 🆓',
+    link: 'https://www.netlify.com/'
+  },
+  {
+    name: 'Heroku 🆓',
+    link: 'https://www.heroku.com/'
+  },
+  {
+    name: 'AWS Amazon 🆓',
+    link: 'https://aws.amazon.com/'
+  },
+  {
+    name: 'Firebase 🆓',
+    link: 'https://firebase.google.com/'
+  },
+  {
+    name: 'GitlabPages',
+    link: 'https://docs.gitlab.com/ee/user/project/pages/'
+  },
+  {
+    name: 'Render 🆓',
+    link: 'https://render.com/'
+  },
+  {
+    name: 'Railway 🆓',
+    link: 'https://railway.app/'
+  },
+  {
+    name: 'Surge 🆓',
+    link: 'https://surge.sh/'
+  }
+]
+
+export const dataPrototyping = [
+  {
+    name: 'Figma',
+    link: 'https://www.figma.com/'
+  },
+  {
+    name: 'Sketch',
+    link: 'https://www.sketch.com/'
+  },
+  {
+    name: 'Webflow',
+    link: 'https://webflow.com/'
+  },
+  {
+    name: 'Adobe',
+    link: 'https://www.adobe.com/'
+  },
+  {
+    name: 'InVision',
+    link: 'https://www.invisionapp.com/studio'
+  }
+]
+
+export const dataCodeChallenges = [
+  {
+    name: 'Codewars',
+    link: 'https://www.codewars.com/'
+  },
+  {
+    name: 'Codingame',
+    link: 'https://www.codingame.com/start'
+  },
+  {
+    name: 'HackerRank',
+    link: 'https://www.hackerrank.com/'
+  },
+  {
+    name: 'LeetCode',
+    link: 'https://leetcode.com/'
+  },
+  {
+    name: 'Sphere online judge',
+    link: 'https://www.spoj.com/'
+  },
+  {
+    name: 'Codesignal',
+    link: 'https://codesignal.com/developers/'
+  },
+  {
+    name: 'FrontendMentor',
+    link: 'https://www.frontendmentor.io/'
+  },
+  {
+    name: 'Exercism',
+    link: 'https://exercism.org/'
+  },
+  {
+    name: 'Codechef',
+    link: 'https://www.codechef.com/'
+  },
+  {
+    name: 'Surge',
+    link: 'https://surge.sh/'
+  },
+  {
+    name: 'Codebyte',
+    link: 'https://coderbyte.com/'
+  },
+  {
+    name: 'Project Euler',
+    link: 'https://projecteuler.net/'
+  },
+  {
+    name: 'Topcoder',
+    link: 'https://www.topcoder.com/'
+  }
+]
+
+export const dataImagesPhotos = [
+  {
+    name: 'Pexels',
+    link: 'https://www.pexels.com/'
+  },
+  {
+    name: 'Pixaby',
+    link: 'https://pixabay.com/'
+  },
+  {
+    name: 'Unsplash',
+    link: 'https://unsplash.com/'
+  },
+  {
+    name: 'Flickr',
+    link: 'https://www.flickr.com/'
+  },
+  {
+    name: 'Burst',
+    link: 'https://burst.shopify.com/'
+  }
+]
+
+export const dataColorPallets = [
+  {
+    name: 'Coolors',
+    link: 'https://coolors.co/'
+  },
+  {
+    name: 'Colorhunt',
+    link: 'https://colorhunt.co/'
+  },
+  {
+    name: 'Color-Hex',
+    link: 'https://www.color-hex.com/'
+  },
+  {
+    name: 'Mycolor',
+    link: 'https://mycolor.space/'
+  },
+  {
+    name: 'Colorable',
+    link: 'https://colorable.jxnblk.com/'
+  },
+  {
+    name: 'Picture-Palette',
+    link: 'https://picture-palette.web.app/'
+  },
+  {
+    name: 'Material-io',
+    link: 'https://material.io/resources/color/#!/?view.left=0&view.right=0'
+  }
+]
+
+export const dataWebIllustrations = [
+  {
+    name: 'Drawkit',
+    link: 'https://drawkit.com/'
+  },
+  {
+    name: 'Icons8',
+    link: 'https://icons8.com/'
+  },
+  {
+    name: 'Icons8 - 3d-Hands',
+    link: 'https://icons8.com/l/3d-hands/#buy-fun'
+  },
+  {
+    name: 'Blush',
+    link: 'https://blush.design/'
+  },
+  {
+    name: 'Interfacer',
+    link: 'https://interfacer.xyz/'
+  },
+  {
+    name: 'Artify Illustrations + Figma',
+    link: 'https://www.artify.co/illustrations-figma'
+  },
+  {
+    name: 'Picture-Palette',
+    link: 'https://picture-palette.web.app/'
+  },
+  {
+    name: 'Material-io',
+    link: 'https://material.io/resources/color/#!/?view.left=0&view.right=0'
+  }
+]
+
+export const dataWebFonts = [
+  {
+    name: 'Google Fonts',
+    link: 'https://fonts.google.com/'
+  },
+  {
+    name: 'Font Squirrel',
+    link: 'https://www.fontsquirrel.com/'
+  },
+  {
+    name: 'Font Space',
+    link: 'https://www.fontspace.com/'
+  },
+  {
+    name: '1001 Fonts',
+    link: 'https://www.1001fonts.com/'
+  }
+]
+
+export const dataOptimization = [
+  {
+    name: 'PageSpeed Insights',
+    link: 'https://pagespeed.web.dev/'
+  },
+  {
+    name: 'GTmetrix',
+    link: 'https://gtmetrix.com/'
+  },
+  {
+    name: 'WebPageTest',
+    link: 'https://www.webpagetest.org/'
+  }
+]
