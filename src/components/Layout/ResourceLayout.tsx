d3623a58d42e4dddbe8d6b939d3374734b94048a
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-06-18 21:41:49 -0300

    feat/refactor-resource-layout

--- src/components/Layout/ResourceLayout.tsx
+++ src/components/Layout/ResourceLayout.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import { LinkIcon } from '@chakra-ui/icons'
-import { Box, Center, Flex, Link, Tag, Text, useColorMode } from '@chakra-ui/react'
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
+import { Box, Center, Collapse, Flex, Link, Tag, Text, useColorMode } from '@chakra-ui/react'
 
 interface ResourceData {
   name: string
@@ -8,38 +10,60 @@ interface ResourceData {
 
 const ResourceLayout = ({ title, data }: { title: string; data: ResourceData[] }) => {
   const { colorMode } = useColorMode()
+  const [show, setShow] = useState(true)
   const settingColor = colorMode === 'light' ? 'rgba(25,0,0,0.1)' : '#27272a'
+  const handleToggle = () => setShow(!show)
+
+  const viewMoreLess = (
+    <Text
+      mb={'2'}
+      mt={'1'}
+      ml={'auto'}
+      align={'center'}
+      cursor={'pointer'}
+      fontSize={'x-small'}
+      onClick={handleToggle}>
+      Ver {show ? 'menos' : 'más'} {show ? <ChevronUpIcon /> : <ChevronDownIcon />}
+    </Text>
+  )
+
   return (
-    <Box
-      m={'2'}
-      ml={{ base: '0', md: '2' }}
-      mr={{ base: '0', md: '2' }}
-      p={'2'}
-      w={'100'}
-      bg={settingColor}
-      borderRadius={'4'}
-      id={title}>
-      <Center>
-        <Tag>{title}</Tag>
-      </Center>
-      <Flex m={'2'} flexDir={'column'}>
-        {data.map((item, index) => (
-          <Box key={index}>
-            <Link href={item.link} target={'_blank'}>
-              <Text
-                m={'1'}
-                borderRadius={'4'}
-                noOfLines={2}
-                w={{ base: '100%', md: '80' }}
-                _hover={{
-                  backgroundColor: colorMode === 'light' ? '#DBDBDB' : 'rgba(5,0,0,0.1)'
-                }}>
-                <LinkIcon /> {item.name}
-              </Text>
-            </Link>
-          </Box>
-        ))}
-      </Flex>
+    <Box>
+      <Collapse startingHeight={50} in={show}>
+        <Box
+          m={'2'}
+          p={'2'}
+          w={'100'}
+          id={title}
+          bg={settingColor}
+          borderRadius={'4'}
+          ml={{ base: '0', md: '2' }}
+          mr={{ base: '0', md: '2' }}>
+          <Center>
+            <Tag>{title}</Tag>
+            {viewMoreLess}
+          </Center>
+          <Flex m={'2'} flexDir={'column'}>
+            {data.map((item, index) => (
+              <Box key={index}>
+                <Link href={item.link} target={'_blank'}>
+                  <Text
+                    m={'1'}
+                    noOfLines={2}
+                    borderRadius={'4'}
+                    w={{ base: '100%', md: '80' }}
+                    _hover={{
+                      backgroundColor: colorMode === 'light' ? '#DBDBDB' : 'rgba(5,0,0,0.1)'
+                    }}>
+                    <LinkIcon /> {item.name}
+                  </Text>
+                </Link>
+              </Box>
+            ))}
+          </Flex>
+          {data.length >= 10 && viewMoreLess}
+        </Box>
+      </Collapse>
     </Box>
   )
 }
