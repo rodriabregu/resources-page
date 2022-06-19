5066998ef35d37496b512a29f2b7b177728b1d4e
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-06-18 21:42:52 -0300

    feat/more-data

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,99 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { ArrowUpIcon } from '@chakra-ui/icons'
+import { Box, Button, Center, Flex } from '@chakra-ui/react'
 import {
   dataBackend,
   dataBootcamp,
+  dataCodeChallenges,
+  dataColorPallets,
   dataFrontend,
   dataGithub,
+  dataHosting,
+  dataImagesPhotos,
   dataJavascript,
   dataLearning,
-  dataRoadmap
-} from 'data/example'
+  dataOptimization,
+  dataPrototyping,
+  dataRoadmap,
+  dataWebFonts,
+  dataWebIllustrations
+} from 'data/resourcing'
 
 import AlertIntro from 'components/AlertIntro'
 import ButtonDarkMode from 'components/ButtonDarkMode'
 import ResourceLayout from 'components/Layout/ResourceLayout'
 import WrapperLayout from 'components/Layout/WrapperLayout'
 import SummarySection from 'components/SummarySection'
+import { scrollToSection } from 'components/ToUpScroll'
+
+const titles: string[] = [
+  'Learning',
+  'Bootcamps',
+  'Backend',
+  'Frontend',
+  'Javascript',
+  'Github (repositorios)',
+  'Paleta de colores',
+  'Roadmap',
+  'Code Challenges',
+  'Hosting',
+  'Prototipo UI/UX',
+  'Ilustración web',
+  'Imagenes/Fotos',
+  'Optimización tools'
+]
 
 function App() {
   return (
-    <Box>
+    <Box id="app">
       <ButtonDarkMode />
       <AlertIntro />
-      <SummarySection />
+      <SummarySection titles={titles} />
       <Flex m={'4'} align={{ md: 'center' }} direction={'column'}>
         <WrapperLayout>
           <ResourceLayout title={'Learning'} data={dataLearning} />
-          <ResourceLayout title={'Javascript'} data={dataJavascript} />
-        </WrapperLayout>
-
-        <WrapperLayout>
-          <ResourceLayout title={'Javascript'} data={dataJavascript} />
-          <ResourceLayout title={'Javascript'} data={dataJavascript} />
-        </WrapperLayout>
-
-        <WrapperLayout>
           <ResourceLayout title={'Bootcamps'} data={dataBootcamp} />
-          <ResourceLayout title={'Github'} data={dataGithub} />
-        </WrapperLayout>
-
-        <WrapperLayout>
-          <ResourceLayout title={'Assets'} data={dataBootcamp} />
-          <ResourceLayout title={'Roadmap'} data={dataRoadmap} />
         </WrapperLayout>
 
         <WrapperLayout>
           <ResourceLayout title={'Backend'} data={dataBackend} />
           <ResourceLayout title={'Frontend'} data={dataFrontend} />
         </WrapperLayout>
+
+        <WrapperLayout>
+          <ResourceLayout title={'Javascript'} data={dataJavascript} />
+          <ResourceLayout title={'Github (repositorios)'} data={dataGithub} />
+        </WrapperLayout>
+
+        <WrapperLayout>
+          <ResourceLayout title={'Code challenges'} data={dataCodeChallenges} />
+          <ResourceLayout title={'Hosting'} data={dataHosting} />
+        </WrapperLayout>
+
+        <WrapperLayout>
+          <ResourceLayout title={'Paleta de colores'} data={dataColorPallets} />
+          <ResourceLayout title={'Roadmap'} data={dataRoadmap} />
+        </WrapperLayout>
+
+        <WrapperLayout>
+          <ResourceLayout title={'Prototipo UI/UX'} data={dataPrototyping} />
+          <ResourceLayout title={'Imagenes/Fotos'} data={dataImagesPhotos} />
+        </WrapperLayout>
+
+        <WrapperLayout>
+          <ResourceLayout title={'Ilustración web'} data={dataWebIllustrations} />
+          <ResourceLayout title={'Fuentes web'} data={dataWebFonts} />
+        </WrapperLayout>
+
+        <WrapperLayout>
+          <ResourceLayout title={'Optimización tools'} data={dataOptimization} />
+          {/* <ResourceLayout title={'Fuentes web'} data={dataWebFonts} /> */}
+        </WrapperLayout>
       </Flex>
+      <Center>
+        <Button m={'2'} onClick={() => scrollToSection('app')}>
+          Up {<ArrowUpIcon />}
+        </Button>
+      </Center>
     </Box>
   )
 }
